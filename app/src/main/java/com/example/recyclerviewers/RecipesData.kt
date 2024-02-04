3cd86708f02f9fd9dfbadd235ad9ef2558ed4e7c
Aurellie Azzahra <aurellieazzahra07@gmail.com>
2024-02-04 19:19:50 +0700

    feat: add more recipes, change toolbar color

--- app/src/main/java/com/example/recyclerviewers/RecipesData.kt
+++ app/src/main/java/com/example/recyclerviewers/RecipesData.kt
@@ -53,6 +53,101 @@ object RecipesData {
                     "1/2 sdt ketumbar\n" +
                     "1 sdt garam",
             "https://upload.wikimedia.org/wikipedia/commons/7/71/Resep_cara_membuat_semur_jengkol_pedas_yang_yummy.jpg"
+        ),
+        arrayOf(
+            "Cumi Tepung",
+            "500 gr cumi-cumi, dipotong ring tebal 1cm\n" +
+                    "1/2 sendok teh garam\n" +
+                    "1/4 sendok teh merica bubuk\n" +
+                    "1/2 sendok teh minyak wijen\n" +
+                    "500 ml minyak untuk menggoreng\n" +
+                    "Bahan Pencelup\n" +
+                    "100 gr tepung terigu\n" +
+                    "1/2 sendok makan bawang putih bubuk\n" +
+                    "1/2 sendok teh garam\n" +
+                    "1/4 sendok teh merica bubuk\n" +
+                    "1/2 sendok teh gula pasir\n" +
+                    "1/2 sendok teh baking powder\n" +
+                    "200 ml air es\n" +
+                    "Bahan Pelapis (aduk rata)\n" +
+                    "300 gr tepung terigu\n" +
+                    "2 sendok makan tepung sagu\n" +
+                    "1 sendok teh Royco kaldu ayam bubuk\n" +
+                    "1 sendok teh garam\n" +
+                    "1 sendok makan biji wijen\n" +
+                    "Bahan Saus\n" +
+                    "4 butir bawang merah, iris tipis\n" +
+                    "1 lembar daun salam\n" +
+                    "1/2 sendok teh pala bubuk\n" +
+                    "1 butir cengkeh utuh\n" +
+                    "1 cm kayumanis\n" +
+                    "3 sendok makan Bango kecap manis\n" +
+                    "1/8 sendok teh garam\n" +
+                    "200 ml air\n" +
+                    "1/2 sendok teh maizena dilarutkan ke dalam 1 sendok teh air\n" +
+                    "1 sendok makan minyak untuk menumis\n" +
+                    "Bumbu Halus\n" +
+                    "2 siung bawang putih\n" +
+                    "2 butir kemiri, disangrai\n" +
+                    "1/4 sendok teh ketumbar\n" +
+                    "1/8 sendok teh jintan putih bubuk",
+            "https://img-global.cpcdn.com/recipes/8e9c9d76a37dfb14/400x400cq70/photo.jpg"
+        ), arrayOf(
+            "Semur Ayam",
+            "5 bawang merah\n" +
+                    "5 bawang putih\n" +
+                    "2 cm jahe\n" +
+                    "Bahan\n" +
+                    "400 gr (1/2 ekor) ayam utuh potong 6 bagian, goreng setengah matang\n" +
+                    "3 buah kentang, kupas, potong seukuran kubus 4 cm, goreng setengah matang\n" +
+                    "1 batang serai\n" +
+                    "5 sdm Bango kecap manis\n" +
+                    "1 sachet Royco kaldu ayam (9 gr)\n" +
+                    "1/2 biji pala, diparut\n" +
+                    "1/2 sdt merica bubuk\n" +
+                    "1/4 sdt garam\n" +
+                    "1 liter air\n" +
+                    "3 sdm minyak, untuk menumis\n" +
+                    "2 sdm bawang merah goreng, untuk penyajian",
+            "https://i0.wp.com/resepkoki.id/wp-content/uploads/2017/07/Resep-Semur-Ayam.jpg?fit=1920%2C1913&ssl=1"
+        ), arrayOf(
+            "Semur Tahu Bakso",
+            "3 sendok makan minyak goreng\n" +
+                    "5 butir bawang merah, iris tipis\n" +
+                    "3 siung bawang putih, iris tipis\n" +
+                    "2 cm jahe, iris batang korek api\n" +
+                    "500 ml air atau kaldu ayam atau kaldu sapi\n" +
+                    "5 sendok makan Bango kecap manis\n" +
+                    "1/2 sendok teh lada putih bubuk\n" +
+                    "1 sendok teh garam\n" +
+                    "1 batang daun bawang\n" +
+                    "150 gr (1 buah) brokoli kecil, potong, direbus sebentar\n" +
+                    "150 gr (1 buah) kembang kol, potong, direbus sebentar\n" +
+                    "Tahu bakso\n" +
+                    "4 buah tahu putih ukuran 10x10x5cm\n" +
+                    "200 gr daging ikan tenggiri, haluskan\n" +
+                    "3 sendok makan tepung kanji atau tepung sagu (50 gram)\n" +
+                    "2 siung bawang putih, diparut\n" +
+                    "1 batang daun bawang, iris tipis\n" +
+                    "1 batang seledri, iris tipis\n" +
+                    "1 sendok teh minyak wijen\n" +
+                    "1 sendok teh gula pasir\n" +
+                    "1/2 sendok teh garam",
+            "https://img-global.cpcdn.com/recipes/f3f4df53541547c6/400x400cq70/photo.jpg"
+        ), arrayOf(
+            "Udang Saus Tiram",
+            "500 g udang, bersihkan dengan dikupas kulitnya, kemudian belah punggunya\n" +
+                    "1 sdt garam\n" +
+                    "1/2 sdt merica bubuk\n" +
+                    "4 sdm mentega / margarin\n" +
+                    "5 siung bawang putih, cincang halus\n" +
+                    "1 buah bawang bombay, iris tipis\n" +
+                    "6 sdm saus tiram\n" +
+                    "4 sdm Bango kecap manis\n" +
+                    "2 sdm kecap asin\n" +
+                    "3 sdm minyak wijen\n" +
+                    "50 cc air",
+            "https://www.masakapahariini.com/wp-content/uploads/2022/01/DSC07967-Low-res-500x300.jpg"
         )
     )
     val listData: ArrayList<Recipes>
